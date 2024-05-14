bae58e842de449c5f605df856f843651f11a9160
kajugadaniels <kajugadaniels@gmail.com>
2024-05-14 10:26:26 +0200

    Name routes

--- backend/urls.py
+++ backend/urls.py
@@ -4,6 +4,8 @@ from .views import *
 from django.conf.urls.static import static
 from django.contrib import admin
 
+app_name = 'backend'
+
 urlpatterns = [
    path('', login, name="login")
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,9 @@ from django.conf import settings
 from .views import *
 from django.conf.urls.static import static
 from django.contrib import admin
+
+app_name = 'frontend'
+
 urlpatterns = [
    path('', index, name="home")
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
