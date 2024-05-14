6c12e0890cd5a03cd13b730df9a4b4c21ee04ca9
kajugadaniels <kajugadaniels@gmail.com>
2024-05-14 09:54:29 +0200

    Install backend app

---
+++ backend/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from .views import *
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+   path('', login, name="login")
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'AOF Administration'                  
+admin.site.index_title = 'Dashboard'                
+admin.site.site_title = 'AOF Admin'
