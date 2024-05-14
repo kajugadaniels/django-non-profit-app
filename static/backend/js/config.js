b6c72f1a6794b9e86615633649c304dc8707d977
kajugadaniels <kajugadaniels@gmail.com>
2024-05-14 16:39:05 +0200

    Design admin dashboard

--- static/backend/js/config.js
+++ static/backend/js/config.js
@@ -2,7 +2,7 @@
   var primary = localStorage.getItem("primary") || "#15548C";
   var secondary = localStorage.getItem("secondary") || "#48A3D7";
 
-  window.OasAdminConfig = {
+  window.AosAdminConfig = {
     // Theme Primary Color
     primary: primary,
     // theme secondary color
