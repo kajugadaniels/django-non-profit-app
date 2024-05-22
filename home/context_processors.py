22d9132a4e5b2a53dbba76b96c2c88c121949995
kajugadaniels <kajugadaniels@gmail.com>
2024-05-22 18:25:04 +0200

    Fixing errors

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,6 +1,11 @@
 def cart_details(request):
     cart = request.session.get('cart', [])
-    total_amount = sum(float(item['amount']) for item in cart)
+    total_amount = 0
+    for item in cart:
+        try:
+            total_amount += float(item['amount'])
+        except ValueError:
+            pass
     return {
         'cart': cart,
         'total_amount': total_amount,
