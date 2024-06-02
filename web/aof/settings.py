71fa7ba71b30f7794ce57e544b06100a44f664fa
Telesphore <30042771+tuganimana@users.noreply.github.com>
2024-06-02 09:39:03 +0200

    remove unwanted tfile

--- web/aof/settings.py
+++ web/aof/settings.py
@@ -156,8 +156,7 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 WHITENOISE_AUTOREFRESH = True
 
 # Default primary key field type
