6194c830edbe718e471ad5a99c1e07c8a0146bf9
kajugadaniels <kajugadaniels@gmail.com>
2024-06-03 15:06:53 +0200

    New changes

--- backend/forms.py
+++ backend/forms.py
@@ -7,13 +7,13 @@ class StudentForm(forms.ModelForm):
 
     class Meta:
         model = Student
-        fields = ['name', 'image', 'birthday', 'gender', 'description']
+        fields = ['name', 'image', 'birthday', 'gender', 'spo_cover', 'description']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Student Name'}),
             'image': forms.FileInput(attrs={'class': 'form-control'}),
             'birthday': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'gender': forms.Select(attrs={'class': 'form-select'}, choices=Student.GENDER_CHOICES),
-            # 'spo_cover': forms.Select(attrs={'class': 'form-select'}, choices=Student.SPONSORSHIP_COVER_CHOICES),
+            'spo_cover': forms.Select(attrs={'class': 'form-select'}, choices=Student.SPONSORSHIP_COVER_CHOICES),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Additional Information'}),
         }
 
