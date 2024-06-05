c763dbc6bd5ae0aa535c7245543069bbd5eb0429
Telesphore <30042771+tuganimana@users.noreply.github.com>
2024-06-06 01:06:24 +0200

    add return on moddels

--- home/models.py
+++ home/models.py
@@ -50,7 +50,7 @@ class Donate(models.Model):
             self.slug = f"{base_slug}-{random_string}"
         super().save(*args, **kwargs)
     def __str__(self) -> str:
-        return self.donationTitle
+        return self.email
 
 class DonateToStudents(models.Model):
     donationTitle= models.CharField(max_length=255)
@@ -73,7 +73,7 @@ class DonateToStudents(models.Model):
             self.slug = f"{base_slug}-{random_string}"
         super().save(*args, **kwargs)
     def __str__(self) -> str:
-        return self.donationTitle
+        return self.email
 
 class Donation(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
