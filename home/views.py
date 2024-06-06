3d6ba8b2eba9df9d1d1d290fc8e7e9682326a14a
kajugadaniels <kajugadaniels@gmail.com>
2024-06-06 16:26:10 +0200

    Final commit

--- home/views.py
+++ home/views.py
@@ -538,7 +538,7 @@ def viewCampaign(request, slug):
             fundraising.campaign = campaign
             fundraising.save()
             messages.success(request, 'Your fundraising request has been submitted successfully.')
-            return redirect(f'/campaigns/{slug}')
+            return redirect(f'/campaigns/{slug}?success=true')
     else:
         form = FundraisingForm()
 
