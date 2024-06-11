d67460ecba24c24d93aeb9428d3acfb15c6c755c
Telesphore <30042771+tuganimana@users.noreply.github.com>
2024-06-12 01:12:14 +0200

    changes  data

--- backend/models.py
+++ backend/models.py
@@ -136,8 +136,8 @@ class Project(models.Model):
         format='JPEG',
         options={'quality': 90},
     )
-    target = models.CharField(max_length=255, default="Project")
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default="Regular Projects")
+    # target = models.CharField(max_length=255, default="Project")
+    # category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default="Regular Projects")
     description = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
